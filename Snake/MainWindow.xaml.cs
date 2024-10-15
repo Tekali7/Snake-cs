723424ad7655338f72b1fd59b505fc9adf5d7dbc
Tekali7 <144740817+Tekali7@users.noreply.github.com>
2024-10-15 22:53:51 +0200

    Add googly eyes to snake head with image rotation according to moving direction

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -23,6 +23,14 @@ namespace Snake
             { GridValue.Food, Images.Food }
         };
 
+        private readonly Dictionary<Direction, int> dirToRotation = new()
+        {
+            { Direction.Up, 0 },
+            { Direction.Right, 90 },
+            { Direction.Down, 180 },
+            { Direction.Left, 270 }
+        };
+
         private readonly int rows = 15, cols = 15;
         private readonly Image[,] gridImages;
         private GameState gameState;
@@ -120,6 +128,7 @@ namespace Snake
         private void Draw()
         {
             DrawGrid();
+            DrawSnakeHead();
             ScoreText.Text = $"SCORE {gameState.Score}";
         }
 
@@ -133,6 +142,16 @@ namespace Snake
                     gridImages[r, c].Source = gridValtoImage[gridVal];
                 }
             }
+        }
+
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
         }
 
         private async Task ShowCountDown()
